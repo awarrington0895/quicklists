a4088a1884191bf84d74abe7dde8ebe8ff894d3c
Alex <awarrington0895@gmail.com>
2024-03-08 18:28:36 +0000

    Complete lesson 11
    Add persistence to local storage

--- src/app/checklist/data-access/checklist-item.service.ts
+++ src/app/checklist/data-access/checklist-item.service.ts
@@ -1,25 +1,32 @@
-import { Injectable, computed, signal } from "@angular/core";
+import { Injectable, computed, effect, inject, signal } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { Subject } from "rxjs";
+import { StorageService } from "../../shared/data-access/storage.service";
 import { RemoveChecklist } from "../../shared/interfaces/checklist";
 import { AddChecklistItem, ChecklistItem, RemoveChecklistItem } from "../../shared/interfaces/checklist-item";
 
 export interface ChecklistItemsState {
     checklistItems: ChecklistItem[];
+    loaded: boolean,
+    error: string | null;
 }
 
 @Injectable({
     providedIn: 'root'
 })
 export class ChecklistItemService {
+    private storageService = inject(StorageService);
 
     // State
     private state = signal<ChecklistItemsState>({
-        checklistItems: []
+        checklistItems: [],
+        loaded: false,
+        error: null
     });
 
     // Selectors
     checklistItems = computed(() => this.state().checklistItems);
+    loaded = computed(() => this.state().loaded);
 
     // Sources
     add$ = new Subject<AddChecklistItem>();
@@ -27,6 +34,8 @@ export class ChecklistItemService {
     toggle$ = new Subject<RemoveChecklistItem>();
 
     reset$ = new Subject<RemoveChecklist>();
+
+    private checklistItemsLoaded$ = this.storageService.loadChecklistItems();
 
     constructor() {
         this.add$
@@ -69,5 +78,23 @@ export class ChecklistItemService {
                     )
                 }))
             );
+
+        this.checklistItemsLoaded$
+            .pipe(takeUntilDestroyed())
+            .subscribe({
+                next: checklistItems =>
+                    this.state.update(state => ({
+                        ...state,
+                        checklistItems,
+                        loaded: true
+                    })),
+                error: err => this.state.update(state => ({ ...state, error: err }))
+            });
+
+        effect(() => {
+            if (this.loaded()) {
+                this.storageService.saveChecklistItems(this.checklistItems());
+            }
+        })
     }
 }
--- src/app/shared/data-access/checklist.service.ts
+++ src/app/shared/data-access/checklist.service.ts
@@ -1,24 +1,35 @@
-import { Injectable, computed, signal } from '@angular/core';
+import { Injectable, computed, effect, inject, signal } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Subject } from 'rxjs';
 import { AddChecklist, Checklist } from '../interfaces/checklist';
+import { StorageService } from './storage.service';
 
 export interface ChecklistsState {
   checklists: Checklist[];
+  loaded: boolean;
+  error: string | null;
 }
 
 @Injectable({ providedIn: 'root' })
 export class ChecklistService {
+  private storageService = inject(StorageService);
+
   // state
   private state = signal<ChecklistsState>({
     checklists: [],
+    loaded: false,
+    error: null
   });
+
+  loaded = computed(() => this.state().loaded);
 
   // selectors
   checklists = computed(() => this.state().checklists);
 
   // sources
   add$ = new Subject<AddChecklist>();
+
+  private checklistsLoaded$ = this.storageService.loadChecklists();
 
   constructor() {
     this.add$.pipe(takeUntilDestroyed()).subscribe((checklist) =>
@@ -27,6 +38,24 @@ export class ChecklistService {
         checklists: [...state.checklists, this.addIdToChecklist(checklist)],
       }))
     );
+
+    this.checklistsLoaded$
+      .pipe(takeUntilDestroyed())
+      .subscribe({
+        next: checklists =>
+          this.state.update(state => ({
+            ...state,
+            checklists,
+            loaded: true
+          })),
+        error: err => this.state.update(state => ({ ...state, error: err }))
+      });
+
+    effect(() => {
+      if (this.loaded()) {
+        this.storageService.saveChecklists(this.checklists());
+      }
+    })
   }
 
   private addIdToChecklist(checklist: AddChecklist) {
@@ -41,13 +70,13 @@ export class ChecklistService {
 
     // Check if the slug already exists
     const matchingSlugs = this.checklists().find(
-        checklist => checklist.id === slug
+      checklist => checklist.id === slug
     );
 
     // if title being used, add a string to make unique
 
     if (matchingSlugs) {
-        slug = slug + Date.now().toString();
+      slug = slug + Date.now().toString();
     }
 
     return slug;
